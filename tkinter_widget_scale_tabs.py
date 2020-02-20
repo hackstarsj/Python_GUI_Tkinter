64b2b111c5d8ac3c36cdc85d9fa54cf4b608a944
sanjeev kumar <sksanjeev362@gmail.com>
2020-02-20 14:00:32 +0530

    Added Table Edit and Update Data

--- tkinter_widget_scale_tabs.py
+++ tkinter_widget_scale_tabs.py
@@ -1,4 +1,4 @@
-from tkinter import Scale,Tk,Frame,Label
+from tkinter import Scale, Tk, Frame, Label, Button
 from tkinter.ttk import Notebook,Entry
 
 def getValue(value):
@@ -48,19 +48,48 @@ tab1=Frame(tablayout)
 tab1.pack(fill="both")
 
 #adding table into tab
+def showData(btn):
+    row=btn.grid_info()['row']
+    column=btn.grid_info()['column']
+    print("Column : "+str(column)+" Row : "+str(row))
+    widget=tab1.grid_slaves(row=row,column=0)[0]
+    widget2=tab1.grid_slaves(row=row,column=1)[0]
+    widget3=tab1.grid_slaves(row=row,column=2)[0]
+    widget4=tab1.grid_slaves(row=row,column=3)[0]
+    print("Value at Column 1 : "+widget.cget("text") +" Column 2 : "+widget2.cget("text") + " Column 3 : "+widget3.cget("text")+" Column 4 : "+widget4.cget("text"))
+
+    #updating value of label
+    widget.config(text="New Data Click")
+
 
 for row in range(5):
     for column in range(6):
         if row==0:
-            label = Label(tab1, text="Heading : " + str(column), bg="white", fg="black", padx=3,
-                          pady=3)
-            label.config(font=('Arial',14))
-            label.grid(row=row, column=column, sticky="nsew", padx=1, pady=1)
-            tab1.grid_columnconfigure(column, weight=1)
+            if column==5:
+                label = Label(tab1, text="Action", bg="white", fg="black", padx=3, pady=3)
+                label.config(font=('Arial', 14))
+                label.grid(row=row, column=column, sticky="nsew", padx=1, pady=1)
+                tab1.grid_columnconfigure(column, weight=1)
+            else:
+                label = Label(tab1, text="Heading : " + str(column), bg="white", fg="black", padx=3, pady=3)
+                label.config(font=('Arial', 14))
+                label.grid(row=row, column=column, sticky="nsew", padx=1, pady=1)
+                tab1.grid_columnconfigure(column, weight=1)
+
         else:
-            label=Label(tab1,text="Row : "+str(row)+" , Column : "+str(column),bg="black",fg="white",padx=3,pady=3)
-            label.grid(row=row,column=column,sticky="nsew",padx=1,pady=1)
-            tab1.grid_columnconfigure(column,weight=1)
+            if column==5:
+                button=Button(tab1,text="Edit",bg="blue",fg="white",padx=3,pady=3)
+                button.grid(row=row,column=column,sticky="nsew",padx=1,pady=1)
+                button['command']=lambda btn=button:showData(btn)
+                tab1.grid_columnconfigure(column,weight=1)
+            else:
+                label=Label(tab1,text="Data "+str(row)+" "+str(column),bg="black",fg="white",padx=3,pady=3)
+                label.grid(row=row,column=column,sticky="nsew",padx=1,pady=1)
+                tab1.grid_columnconfigure(column,weight=1)
+
+
+
+
 
 tablayout.add(tab1,text="TAB 1")
 
